4a7bea4dd650bbebb93b211ce107152af233fdf8
Pavel N <trudi2004@yandex.ru>
2024-11-08 17:29:32 +0300

    Добавление тестовых данных. Отредактирована структура папки шаблонов, реализован переход к конкретному вопросу

--- app/urls.py
+++ app/urls.py
@@ -8,6 +8,6 @@ app_patterns = [
     path("login/", views.login_page, name="login_page"),
     path("register/", views.registration_page, name="register_page"),
     path("settings/", views.user_settings_page, name="settings_page"),
-    path("question/", views.question_page, name="question_page"),
+    path("question/<int:question_id>", views.question_page, name="question_page"),
     path("tag/", views.tag_page, name="tag_page")
 ]
--- app/views.py
+++ app/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
+from random import randint
 
+# ! Для тестов без базы данных
+QUESTIONS = [
+    {
+        "id": i,
+        "title": f"Вопрос №{i}",
+        "answer_count": randint(0, 1000),
+        "text": f"Здесь располагается основной текст вопроса №{i}, описывающий проблематику задачи",
+        "tags": [f"Тэг №{j}-{i}" for j in range(1, 6)] 
+    } for i in range(1, 21)
+]
 
-def main_page(request):    
-    return render(request, "index.html", {})
+def main_page(request):
+    return render(request, "index.html", {
+        "questions": QUESTIONS
+    })
 
 def ask_page(request):
     return render(request, "ask.html", {})
@@ -16,8 +30,18 @@ def registration_page(request):
 def user_settings_page(request):
     return render(request, "settings.html", {})
 
-def question_page(request):
-    return render(request, "question.html", {})
+def question_page(request, question_id):
+    current_question = QUESTIONS[question_id-1]
+    return render(request, "question.html", {
+        "question": current_question
+    })
 
 def tag_page(request):
-    return render(request, "tag.html", {})
+    return render(request, "tag.html", {
+        "questions": QUESTIONS
+    })
+
+def paginate(objects_list, request, per_page=10):
+    # do smth with Paginator, etc…
+    #return page
+    pass
