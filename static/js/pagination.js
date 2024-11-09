67bfd9b9c2f1ba7009737482e421a402a3773142
Pavel N <trudi2004@yandex.ru>
2024-11-09 17:28:30 +0300

    Редактирование функции pagination_load

--- static/js/pagination.js
+++ static/js/pagination.js
@@ -1,4 +1,6 @@
 // TODO: доделать динамический сдвиг троеточия
+// ? Оптимизировать внешний вид исходного кода 
+// ? (вынести new_list_iewm и new_link во внешнюю область)
 
 function pagination_load(page_count, active_page_number) {
     // количество номеров страниц при пагинации, допускающее отсутсвие троеточия
@@ -10,11 +12,17 @@ function pagination_load(page_count, active_page_number) {
         new_list_item.className = "page-item"
         let new_link = document.createElement("a")
         new_link.className = "page-link"
-        new_link.href = "?page=" + page_number
         new_link.textContent = page_number
 
-        if (page_number == active_page_number) {
-            new_list_item.className += " active"
+        if (page_number == "...") {
+            new_link.href = "" 
+            new_link.textContent = "..."
+        } else {
+            new_link.href = "?page=" + page_number
+
+            if (page_number == active_page_number) {
+                new_list_item.className += " active"
+            }
         }
 
         new_list_item.appendChild(new_link)
@@ -29,16 +37,7 @@ function pagination_load(page_count, active_page_number) {
         for (let page_number = 1; page_number <= pagination_line_length - 1; page_number++) {
             add_page_number(page_number)
         }
-
-        let new_list_item = document.createElement("li");
-        new_list_item.className = "page-item"
-        let new_link = document.createElement("a")
-        new_link.className = "page-link"
-        new_link.href = "" 
-        new_link.textContent = "..."
-        new_list_item.appendChild(new_link)
-        pageList.appendChild(new_list_item)
-
+        add_page_number("...")
         add_page_number(page_count)
     }
 }
